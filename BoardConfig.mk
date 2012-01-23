5f077457c49cf022ea516ad73e462f179fc2776f
xoomdev <xoomdevnet@gmail.com>
2012-01-23 17:51:02 -0600

    samsung/tuna: Add recovery_keys.c to fix skipping highlight in recovery.
    Change-Id: Ie5a1286cd6fbbded86064e8589b8e38f5d4f3448

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,8 @@ BOARD_LIB_DUMPSTATE := libdumpstate.tuna
 BOARD_USES_SECURE_SERVICES := true
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_XHDPI_RECOVERY := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/tuna/recovery_keys.c
 
 BOARD_SEPOLICY_DIRS := \
         device/samsung/tuna/sepolicy
