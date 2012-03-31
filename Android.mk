c32a5d8530c97311262bff61937e889c617423fe
KhasMek <Boushh@gmail.com>
2012-04-01 01:38:56 +0800

    add device checks to prevent other devices from accidentally calling device specific settings

--- Android.mk
+++ Android.mk
@@ -22,8 +22,10 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
 
+ifeq ($(TARGET_DEVICE), tuna)
+
+ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_MODULE := tuna_hdcp_keys
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := FAKE
@@ -44,3 +46,6 @@ $(LOCAL_BUILT_MODULE):
 	$(hide) touch $@
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+
+endif
