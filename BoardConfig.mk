a1e0c6c299b2eac7428e52a91c6a5c784152d4ac
Michael Limb <something15525@gmail.com>
2013-11-12 01:34:21 -0800

    tuna: update flag for dumpstate library
    This is the new standard for HAL static libraries in 4.4
    
    Change-Id: If8d931facd3ec6d48b769641c6c9a15a30ed6529

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,7 @@ WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-BOARD_LIB_DUMPSTATE := libdumpstate.tuna
+BOARD_HAL_STATIC_LIBRARIES := libdumpstate.tuna
 
 BOARD_USES_SECURE_SERVICES := true
 
